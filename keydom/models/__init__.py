6b9af7159eddaad47c34d42d6e72a38e97a9f0a7
Sean Johnson <pirogoeth@maio.me>
2015-09-10 22:40:47 -0500

    0.1: scaffolding for the project

---
+++ keydom/models/__init__.py
@@ -0,0 +1,59 @@
+from keydom.util import parse_uri
+import peewee, playhouse
+
+
+database_proxy = peewee.Proxy()
+
+class FKSqliteDatabase(peewee.SqliteDatabase):
+    """ A simple wrapper around peewee's SqliteDatabase that
+        enables foreign keys with a pragma when the connection
+        is initialized.
+    """
+
+    def initialize_connection(self, conn):
+
+        self.execute_sql("PRAGMA foreign_keys=ON;")
+
+
+class BaseModel(peewee.Model):
+    """ Simple base model with the database set as a peewee
+        database proxy so we can dynamically initialize the
+        database connection with information from the config
+        file.
+    """
+
+    class Meta:
+        database = database_proxy
+
+
+def init_database_from_config(db_config):
+    """ Takes a malibu ConfigurationSection object to create
+        the database connection accordingly.
+    """
+
+    db_uri = db_config.get_string("uri", None)
+    if db_uri is None:
+        raise ValueError("Config value database.uri can not be empty!")
+
+    db_uri = parse_uri(db_uri)
+
+    if db_uri["protocol"] == "sqlite":
+        database = peewee.SqliteDatabase(db_uri["host"])
+    elif db_uri["protocol"] == "postgres":
+        database = playhouse.postgres_ext.PostgresqlExtDatabase(
+            db_uri["database"],
+            user = db_uri["username"],
+            password = db_uri["password"],
+            host = db_uri["host"],
+            port = db_uri["port"])
+    elif db_uri["protocol"] == "mysql":
+        database = peewee.MySQLDatabase(
+            db_uri["database"],
+            user = db_uri["username"],
+            password = db_uri["password"],
+            host = db_uri["host"],
+            port = db_uri["port"])
+    else:
+        raise ValueError("Unknown DB protocol: %s" % (db_uri["protocol"]))
+
+    database_proxy.initialize(database)
---
+++ keydom/util/__init__.py
@@ -0,0 +1,69 @@
+import urlparse
+
+
+def parse_uri(uri):
+    """ This implies that we are passed a uri that looks something like:
+          proto://username:password@hostname:port/database
+
+        In most cases, you can omit the port and database from the string:
+          proto://username:password@hostname
+
+        Also, in cases with no username, you can omit that:
+          proto://:password@hostname:port/database
+
+        Also supports additional arguments:
+          proto://hostname:port/database?arg1=val&arg2=vals
+    """
+
+    proto = uri.split('://')[0]
+    uri = uri.split('://')[1]
+
+    _host = uri.split('@')[-1]
+    _host = _host.split(':')
+    if len(_host) == 2:
+        host = _host[0]
+        if '/' in _host[1]:
+            port = int(_host[1].split('/')[0])
+        else:
+            port = int(_host[1])
+    else:
+        host = _host[0]
+        if '/' in host:
+            host = host.split('/')[0]
+        port = None
+
+    if "@" in uri:
+        _cred = uri[0:uri.rfind(':'.join(_host)) - 1]
+        _cred = _cred.split(':')
+        if len(_cred) == 2:
+            _user = _cred[0]
+            _pass = _cred[1]
+        else:
+            _user = _cred[0]
+            _pass = None
+    else:
+        _user = None
+        _pass = None
+
+    database = uri.split('/')
+    if len(database) >= 2:
+        database = database[1]
+        if '?' in database:
+            _db = database.split('?')
+            database = _db[0]
+            args = urlparse.parse_qs(_db[1], keep_blank_values = True)
+        else:
+            args = None
+    else:
+        database = None
+        args = None
+
+    return {
+        "protocol": proto,
+        "host": host,
+        "port": port,
+        "username": _user,
+        "password": _pass,
+        "database": database,
+        "args": args,
+        "uri": "{}://{}".format(proto, uri)}
